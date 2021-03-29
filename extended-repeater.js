fe19e6ffaaca527087a37e582cfd392fad864c10
Игорь Званько <38100703+referee12345@users.noreply.github.com>
2021-03-29 22:44:08 +0300

    Delete extended-repeater.js

--- extended-repeater.js
+++
@@ -1,7 +0,0 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
